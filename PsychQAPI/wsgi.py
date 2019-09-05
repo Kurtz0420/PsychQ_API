a896bc0eb22f8e5697924d605eae3530afb4aabe
ksajid505@gmail.com <ksajid505@gmail.com>
2019-09-05 07:13:04 +0500

    Correct Wsgi file

--- PsychQAPI/wsgi.py
+++ PsychQAPI/wsgi.py
@@ -7,19 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-# +++++++++++ DJANGO +++++++++++
-# To use your own django app use code like this:
 import os
-import sys
 
-# assuming your django settings file is at '/home/skurtz00420/mysite/mysite/settings.py'
-# and your manage.py is is at '/home/skurtz00420/mysite/manage.py'
-path = '/home/skurtz00420/mysite'
-if path not in sys.path:
-   sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
-# then:
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PsychQAPI.settings")
+
 application = get_wsgi_application()
